235e1360833d260d444c597b76495c52cde7b0b9
leasiond <1acaunt002@gmail.com>
2024-07-23 21:08:32 +0500

    update

--- Create Account/script.js
+++ Create Account/script.js
@@ -1,6 +1,13 @@
-const password = document.getElementById("password")
-const rePassword = document.getElementById("rePassword")
+const passwordsBlock = document.getElementById("passwordsBlock")
 
 const handelSubmit = () => {
-  console.log(e);
+  const password = document.getElementById("password")
+  const rePassword = document.getElementById("rePassword")
+  const errorMessage = document.getElementById("errorMessage")
+  if(password.value === rePassword.value) {
+    errorMessage.style.display = "none"
+  } else {
+    passwordsBlock.style.color = "red"
+    errorMessage.style.display = "block"
+  }
 }
--- script.js
+++ script.js
@@ -1,17 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const burgerIcon = document.getElementById('burger-icon');
-  const mobileMenu = document.getElementById('mobile-menu');
-  const overlay = document.getElementById('overlay');
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerIcon = document.getElementById("burger-icon");
+  const mobileMenu = document.getElementById("mobile-menu");
+  const overlay = document.getElementById("overlay");
 
-  burgerIcon.addEventListener('click', function() {
-    mobileMenu.classList.toggle('open');
-    overlay.classList.toggle('open');
+  burgerIcon.addEventListener("click", function () {
+    mobileMenu.classList.toggle("open");
+    overlay.classList.toggle("open");
   });
 
-  document.addEventListener('click', function(event) {
-    if (!mobileMenu.contains(event.target) && !burgerIcon.contains(event.target)) {
-      mobileMenu.classList.remove('open');
-      overlay.classList.remove('open');
+  document.addEventListener("click", function (event) {
+    if (
+      !mobileMenu.contains(event.target) &&
+      !burgerIcon.contains(event.target)
+    ) {
+      mobileMenu.classList.remove("open");
+      overlay.classList.remove("open");
     }
   });
+});
+
+document.getElementById("copyButton").addEventListener("click", function () {
+  const textToCopy = document.getElementById("textToCopy").innerHTML;
+  const tempElement = document.createElement("textarea");
+  tempElement.value = textToCopy;
+  document.body.appendChild(tempElement);
+
+  tempElement.select();
+  document.execCommand("copy");
+
+  document.body.removeChild(tempElement);
+  const successMsg = document.getElementById("successMessage");
+  successMsg.classList.toggle("open");
+  setTimeout(() => {
+    successMsg.classList.toggle("open");
+  }, 1000);
+});
+
+document.getElementById("followButton").addEventListener("click", () => {
+  const followText = document.getElementById("followText")
+  if(followText.innerHTML === "Followed") {
+    document.getElementById("followSvg").style.display = "inline";
+    followText.innerHTML = "Follow";
+  }else {
+    followText.innerHTML = "Followed";
+    document.getElementById("followSvg").style.display = "none";
+  }
 });
